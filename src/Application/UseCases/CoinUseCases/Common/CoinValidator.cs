92f723c21ff7133d8abae294319a62c7d59dbb22
gcmorais <guilhermeclarodemorais@gmail.com>
2024-09-16 20:41:12 -0300

    refactor(Coin): implementing coin validator

--- src/Application/UseCases/CoinUseCases/Common/CoinValidator.cs
+++ src/Application/UseCases/CoinUseCases/Common/CoinValidator.cs
@@ -1,10 +1,14 @@
-﻿namespace Application.UseCases.CoinUseCases.Common
+﻿using Application.UseCases.CoinUseCases.CreateCoin;
+using FluentValidation;
+
+namespace Application.UseCases.CoinUseCases.Common
 {
-    public sealed class CoinValidator
+    public sealed class CoinValidator : AbstractValidator<CreateCoinRequest>
     {
         public CoinValidator()
         {
-            
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(3).MaximumLength(20);
+            RuleFor(x => x.Abbreviation).NotEmpty().MinimumLength(3).MaximumLength(3);
         }
     }
 }
